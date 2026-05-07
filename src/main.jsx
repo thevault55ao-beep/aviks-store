80b4a84d487251ea348e93bd6672ef45f2750b28
thevault55ao-beep <the.vault55.ao@gmail.com>
2026-05-07 10:16:40 -0300

    Refactor JSX to use React.createElement

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(App, null)
+  )
 )
